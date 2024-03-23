61bc8e5e2496911fbcb7570e7277ed74bc66e5b3
Tim O'Connell <tim@exxo.sh>
2024-03-23 23:36:41 +0000

    fix more terminal settings

--- src/.config/fish/linux.fish
+++ src/.config/fish/linux.fish
@@ -3,8 +3,11 @@ source ~/.config/fish/common.fish
 set -x PNPM_HOME $HOME/.local/share/pnpm
 set -x PATH $PATH $PNPM_HOME
 
-# perl (e.g. git) doesnt like en_GB
+# perl (e.g. git) & nvim doesnt like en_GB
+set -x LANGUAGE en_US.UTF-8
 set -x LANG en_US.UTF-8
+set -x LC_ALL en-US.UTF-8
+set -x LC_CTYPE en-US.UTF-8
 
 alias bat="batcat"
 alias python="python3"
