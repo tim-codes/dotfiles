ea680b1e7481b2078eabfaa4fb86244ccba6b907
Tim O'Connell <tim@exxo.sh>
2024-03-23 23:02:35 +0000

    fix perl/git locale setting

--- src/.config/fish/linux.fish
+++ src/.config/fish/linux.fish
@@ -3,5 +3,8 @@ source ~/.config/fish/common.fish
 set -x PNPM_HOME $HOME/.local/share/pnpm
 set -x PATH $PATH $PNPM_HOME
 
+# perl (e.g. git) doesnt like en_GB
+set -x LANG en_US.UTF-8
+
 alias bat="batcat"
 alias python="python3"
